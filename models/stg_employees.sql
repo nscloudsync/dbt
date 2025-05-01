d3c02963a5797f4dcd27c2096963340e08fa68bd
ns cloud <nscloudsync@gmail.com>
2025-05-01 14:39:28 +0000

    w

--- models/stg_employees.sql
+++ models/stg_employees.sql
@@ -1,29 +1,21 @@
-{{
+{{ 
     config(
-        materialized = 'incremental',
-        unique_key = 'EMPLOYEE_ID',
-        incremental_strategy = 'insert_overwrite',
-        partition_by = {'field': 'load_time', 'data_type':'timestamp'}
-    )
-
+        target_schema = 'stg',
+        materialized='incremental', 
+        unique_key='employee_id'
+    ) 
 }}
 
-select 
-    EMPLOYEE_ID,
-    FIRST_NAME,
-    LAST_NAME,
-    EMAIL,
-    PHONE_NUMBER,
-    HIRE_DATE,
-    JOB_ID,
-    SALARY,
-    MANAGER_ID,
-    DEPARTMENT_ID,
-    current_timestamp as LOAD_TIME
-from {{ source('hr', 'src_employees') }} as src
-
-{% if is_incremental() %}
-
-where src.load_time >= dateadd(day, -7, current_timestamp)
-
-{% endif %}
+SELECT
+    employee_id,
+    first_name,
+    last_name,
+    email,
+    phone_number,
+    hire_date,
+    job_id,
+    salary,
+    manager_id,
+    department_id,
+    CURRENT_TIMESTAMP AS load_time
+FROM hr.employees as src
